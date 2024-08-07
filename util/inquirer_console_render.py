3e47444b22dbda1fe0e6752808e15fa73ede6968
sakkyoi <22865542+sakkyoi@users.noreply.github.com>
2024-08-07 19:36:00 +0800

    rewrite inquirer_console_render patch wrapper for less cognitive complexity

--- util/inquirer_console_render.py
+++ util/inquirer_console_render.py
@@ -1,6 +1,7 @@
 from inquirer.render.console import ConsoleRender
 from inquirer.themes import term
 import unicodedata
+from typing import Optional
 
 
 def _print_options(self, render):
@@ -8,44 +9,16 @@ def _print_options(self, render):
         if hasattr(message, "decode"):  # python 2
             message = message.decode("utf-8")
 
-        hint = ""
-        if render.question.hints is not None:
-            hint = render.question.hints.get(message, "")
+        hint = make_hint(message, symbol, render, self.terminal.width)
 
-            symbol_width = count_string_width(symbol)
-            message_width = count_string_width(message)
-            hint_width = count_string_width(hint)
-            space_width = 3  # space between symbol, message and hint
-            # sometimes the last character will be truncated(just not shown),
-            # so we need a buffer to ensure the hint is not truncated
-            buffer_width = 1
-            ellipsis_width = 3  # for the ellipsis("...")
-
-            total_width = symbol_width + message_width + hint_width + space_width + buffer_width
-
-            if message in render.locked:
-                total_width += count_string_width(" (Done)")
-
-            if total_width > self.terminal.width:
-                # calculate how many characters to truncate
-                to_truncate = total_width - self.terminal.width + ellipsis_width
-
-                # truncate the hint
-                while to_truncate > 0:
-                    to_truncate -= count_string_width(hint[-1])
-                    hint = hint[:-1]
-
-                hint += "..."
-
-            if message in render.locked:
-                hint += f"{term.bold_red} (Done)"
-
-        if hint:
-            f = " {color}{s} {m} {h}{t.normal}" if render.question.choices[render.current] == message \
-                else " {color}{s} {t.white}{m}{color} {h}{t.normal}"
-            self.print_line(f, m=message, color=color, s=symbol, h=hint)
+        if not hint:
+            f = " {color}{s} {m}{t.normal}"
+        elif render.question.choices[render.current] == message:
+            f = " {color}{s} {m} {h}{t.normal}"
         else:
-            self.print_line(" {color}{s} {m}{t.normal}", m=message, color=color, s=symbol)
+            f = " {color}{s} {t.white}{m}{color} {h}{t.normal}"
+
+        self.print_line(f, m=message, color=color, s=symbol, h=hint)
 
 
 def count_string_width(string):
@@ -54,6 +27,50 @@ def count_string_width(string):
     """
     # W: Wide, F: Full-width, A: Ambiguous(maybe wide or narrow, depending on the context. consider as wide here)
     return sum(1 + (unicodedata.east_asian_width(c) in 'WFA') for c in string)
+
+
+def make_hint(message, symbol, render, terminal_width):
+    """
+    Make a hint for the message.
+    """
+    # if there is no hint, return None
+    if render.question.hints is None:
+        return ""
+
+    # get the hint for the message
+    hint = render.question.hints.get(message, "")
+
+    # calculate width
+    symbol_width = count_string_width(symbol)
+    message_width = count_string_width(message)
+    hint_width = count_string_width(hint)
+    space_width = 3  # space between symbol, message and hint
+    # sometimes the last character will be truncated(just not shown),
+    # so we need a buffer to ensure the hint is not truncated
+    buffer_width = 1
+    ellipsis_width = 3  # for the ellipsis("...")
+
+    total_width = symbol_width + message_width + hint_width + space_width + buffer_width
+
+    if message in render.locked:
+        total_width += count_string_width(" (Done)")
+
+    to_truncate = total_width - terminal_width + ellipsis_width
+
+    # truncate the hint
+    while to_truncate > 0:
+        to_truncate -= count_string_width(hint[-1])
+        hint = hint[:-1]
+
+    # add the ellipsis if the hint is truncated
+    if total_width > terminal_width:
+        hint += "..."
+
+    # add the "Done" hint if the message is locked(locked means the option is downloaded)
+    if message in render.locked:
+        hint += f"{term.bold_red} (Done)"
+
+    return hint
 
 
 ConsoleRender._print_options = _print_options  # override the method to print hints with options
